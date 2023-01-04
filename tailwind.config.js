a75668182b8e795e32ee4d0ce6535558d6f7b4ec
Martin Ponce <mdp.mclneurodiagnostic@gmail.com>
2023-01-04 10:41:56 -0300

    install reactjs and tailwind css and create first template

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const { clear } = require('@testing-library/user-event/dist/clear')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
