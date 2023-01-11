dfb4a4f04a29b5433f7c63fc44ede7ab563d8e40
Martin Ponce <mdp.mclneurodiagnostic@gmail.com>
2023-01-11 11:44:27 -0300

    complete the oath functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
-import Profile from "./pages/Profile";
+
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
 import {FcGoogle} from "react-icons/fc"
+import { toast } from 'react-toastify';
+import { db } from "../firebase";
+import { useNavigate } from 'react-router-dom';
 
 export default function OAuth() {
+  const navigate = useNavigate()
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // lista de usuarios en database
+
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      
+      if(!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/");
+    } catch (error) {
+      toast.error("No se pudo autorizar con Google");
+    }
+  }
   return (
     <button 
+        type="button" 
+        onClick={onGoogleClick}
         className='flex 
         items-center
         justify-center 
