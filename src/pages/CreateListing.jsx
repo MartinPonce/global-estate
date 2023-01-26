b8620c9e39f5dcfb17cbfa2c2d52794477afe925
Martin Ponce <mdp.mclneurodiagnostic@gmail.com>
2023-01-25 23:14:54 -0300

    complete the listingItem component

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -199,15 +199,15 @@ export default function CreateListing() {
                     </button>
                 </div>
                 <p className="text-lg mt-6 font-semibold">
-                    Nombre
+                    Detalle
                 </p> 
                 <input 
                     type="text" 
                     id="name" 
                     value={name} 
                     onChange= {onChange} 
-                    placeholder="Nombre y apellido de agente e Inmobiliaria" //Property name
-                    maxLength="32"
+                    placeholder="Ej. Casa a estrenar en barrio privado" //Property name
+                    maxLength="42"
                     minLength="10" 
                     required className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded 
                     transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600
