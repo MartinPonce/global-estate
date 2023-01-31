bc364addfdbfb53789e58c80bfa4f0e5b5979481
Martin Ponce <mdp.mclneurodiagnostic@gmail.com>
2023-01-30 22:04:29 -0300

    add share copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -8,12 +8,14 @@ import { db } from '../firebase';
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { EffectFade, Autoplay, Navigation, Pagination } from "swiper";
 import "swiper/css/bundle";
+import { FaShare } from "react-icons/fa"
 
 
 export default function Listing() {
     const params = useParams();
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [shareLinkCopied, setShareLinkCopied] = useState(false)
     SwiperCore.use([Autoplay, Navigation, Pagination]);
     useEffect(() => {
         async function fetchListing() {
@@ -32,7 +34,13 @@ export default function Listing() {
     }
     return (
         <main>
-            <Swiper slidesPerView={1} navigation pagination={{type: "progressbar"}} effect="fade" modules={[EffectFade]} autoplay={{ delay:3000 }}>
+            <Swiper 
+                slidesPerView={1} 
+                navigation 
+                pagination={{type: "progressbar"}} 
+                effect="fade" 
+                modules={[EffectFade]} 
+                autoplay={{ delay:3000 }}>
                 {listing.imgUrls.map((url, index) => (
                     <SwiperSlide key={index}>
                         <div 
@@ -41,11 +49,26 @@ export default function Listing() {
                                 background: `url(${listing.imgUrls[index]}) center no-repeat`,
                                 backgroundSize: "cover"
                             }}>
-                        
                         </div>
                     </SwiperSlide>
                 ))}
             </Swiper>
+            <div className="fixed top-[7%] right-[3%] z-10 bg-white cursor-pointer border-2
+                 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+                onClick={()=>{
+                    navigator.clipboard.writeText(window.location.href)
+                    setShareLinkCopied(true)
+                    setTimeout(() =>{
+                        setShareLinkCopied(false)
+                    }, 2000)
+                }}>
+                <FaShare className="text-lg text-slate-500"/>
+            </div>
+            {shareLinkCopied && (
+                <p className="fixed top-[15%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10">
+                    Link Copiado
+                </p>
+            )}
         </main>
-    )
+    );
 }
